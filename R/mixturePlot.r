e773222608bb8d455f2b27b99b61ad70ae0ffead
Moore <mooredf@umdnj.edu>
2020-07-31 14:07:37 -0400

    update mixturePlot to compute areas between fitted and true mixture proportions using the trapz function in the pracma package

--- R/mixturePlot.r
+++ R/mixturePlot.r
@@ -1,11 +1,16 @@
 #' plot mixture of two compartment profiles
-#' @param Loc1 row number of the matrix Acup for compartment 1
-#' @param Loc2 row number of the matrix Acup for compartment 2
 #' @param mixProtiProtjProp data frame of CPA estimated proportions for each mixture
 #' @param NstartMaterialFractions  Number of fractions that comprise the starting material
+#' @param Loc1  row number of subcellular location 1 of mixture
+#' @param Loc2  row number of subcellular location 2 of mixture
+
+#' @param mix.df  true mixture proportions used to make the  mixtures (from proteinMix)
 #' @param type normalized specific amounts or relative specific amounts
 
-mixturePlot <- function(mixProtiProtjProp, NstartMaterialFractions=6, Loc1, Loc2, xaxisLab=T, yaxisLab=T) {
+mixturePlot <- function(mixProtiProtjProp, NstartMaterialFractions=6, Loc1, Loc2,
+                        mix.df, xaxisLab=T, yaxisLab=T) {
+  # mixtures must be a list of equally spaced proportions
+  # this program assumes exactly eight subcellular compartments
   # set up color and point lists
   loc.list <- names(mixProtiProtjProp)
   col.list <- c("red", "blue", "orange", "darkgreen", "orange", "lightblue", "purple", "green")
@@ -26,14 +31,26 @@ mixturePlot <- function(mixProtiProtjProp, NstartMaterialFractions=6, Loc1, Loc2
   # calculate sum of squares of errors
 
   }
-  area1 <- abs(0.5 + polyarea(x=c(fracList, 1), y=c(mixProtiProtjProp[,Loc1], 0)))
-  area2 <- abs(-0.5 - polyarea(x=c(fracList, 0), y=c(mixProtiProtjProp[,Loc2],0)))
-  area <- area1 + area2
+
+  areaErr <- 0
+  for (kk in 1:ncol(mixProtiProtjProp)) {
+    #kk=1
+    # use trapezoidal rule (trapz from package pracma)
+    areaErr <- areaErr +
+                abs(trapz(fracList, as.numeric(mix.df[,kk]) ) -
+                      trapz(fracList, as.numeric(mixProtiProtjProp[,kk])))
+
+
+
+  }
+  #area1 <- abs(0.5 + polyarea(x=c(fracList, 1), y=c(mixProtiProtjProp[,Loc1], 0)))
+  # <- abs(-0.5 - polyarea(x=c(fracList, 0), y=c(mixProtiProtjProp[,Loc2],0)))
+  #area <- area1 + area2
 
   abline(a=0,b=1, col="gray")
   abline(a=1,b=-1, col="gray")
   titleText <- paste(loc.list[Loc1], "-", loc.list[Loc2])
-  title(paste(titleText, "(", format(round(area,3), nsmall=3), ")"))   # guarantee 3 digits after decimal
+  title(paste(titleText, "(", format(round(areaErr,3), nsmall=3), ")"))   # guarantee 3 digits after decimal
   #plotLables
   #loc.list  col.list pch.list
   #1     Cyto       red        1
