7e8e43e1c6ef4224ff6fade5f38f446651022992
Moore <mooredf@umdnj.edu>
2020-11-13 20:27:19 -0500

    added new version of data

---
+++ R/TannousTMTms2-data.R
@@ -0,0 +1,12 @@
+#' TannousTMTms2
+#'
+#' Spectral level normalized specific amounts from Tannous et al.
+#'
+#' \describe{
+#'   \item{Protein.Gene}{protein identifier}
+#'   \item{Peptide.Sequence}{peptide identifier}
+#'   \item{Spectrum.ID}{unique spectrum identifier
+#'   }
+#' @usage data(TannousTMTms2)
+#' @example str(TannousTMTms2)
+NULL
