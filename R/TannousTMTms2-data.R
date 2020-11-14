0ffd4b887636bdd7ca78b31899676fc90f92741e
Moore <mooredf@umdnj.edu>
2020-11-13 20:38:41 -0500

    rename data

--- R/TannousTMTms2-data.R
+++ R/TannousTMTms2-data.R
@@ -1,12 +1,12 @@
-#' TannousTMTms2
+#' TannousTMTms2spectral
 #'
 #' Spectral level normalized specific amounts from Tannous et al.
 #'
 #' \describe{
 #'   \item{Protein.Gene}{protein identifier}
 #'   \item{Peptide.Sequence}{peptide identifier}
-#'   \item{Spectrum.ID}{unique spectrum identifier
+#'   \item{Spectrum.ID}{unique spectrum identifier}
 #'   }
-#' @usage data(TannousTMTms2)
-#' @example str(TannousTMTms2)
+#' @usage data(TannousTMTms2spectral)
+#' @example str(TannousTMTms2spectral)
 NULL
